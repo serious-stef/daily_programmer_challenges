8ffe142e7ee6890e01d1687d97fe24d7aab339ee
serious-stef <bunter.vogel@mailbox.org>
2022-05-29 17:19:14 +0200

    First Commit

---
+++ gameofthrees.py
@@ -0,0 +1,43 @@
+# Game of Threes - A Coding-Challenge from reddit.com/dailyprogrammer
+# https://www.reddit.com/r/dailyprogrammer/comments/3r7wxz/20151102_challenge_239_easy_a_game_of_threes/
+
+
+divthree = [3, 6, 9]  # Array for checking the digit sum on divisibility by three.
+
+
+# Function for calculating the digit sum.
+def digitsum(number):
+    q = [int(n) for n in str(number)]  # Array for saving all digits.
+    digitsumresult = 0
+    i = 1
+
+    while i <= len(q):  # Starting at "0" add every digit to the digit sum.
+        digitsumresult = digitsumresult + q[i - 1]
+        i += 1
+    return digitsumresult  # Return the result for further usage.
+
+
+# Main function. This is where python plays the "Game of Threes"
+def game(gamenumber):
+    while gamenumber > 1:
+        checker = [digitsum(gamenumber), digitsum(gamenumber + 1), digitsum(gamenumber - 1)]
+        i = 0
+        for p in checker:
+            while p >= 10:
+                p = digitsum(p)  # Why doesn't this change the value of checker[0]/checker[1]/...?
+            checker[i] = p
+            i += 1
+
+            if checker[0] in divthree:
+                print(str(gamenumber) + " 0")
+                gamenumber = int(gamenumber / 3)
+                break
+            elif checker[1] in divthree:
+                print(str(gamenumber) + " 1")
+                gamenumber = int((gamenumber + 1) / 3)
+                break
+            elif checker[2] in divthree:
+                print(str(gamenumber) + " -1")
+                gamenumber = int((gamenumber - 1) / 3)
+                break
+    print(gamenumber)
---
+++ main.py
@@ -0,0 +1,7 @@
+# This is the main function where all the challenges can be run.
+
+import gameofthrees
+
+
+if __name__ == "__main__":
+    gameofthrees.game(31337357)
