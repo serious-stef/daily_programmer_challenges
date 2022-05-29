a04691bdd224f2b2bb5d88d862648deebb51c5e3
serious-stef <bunter.vogel@mailbox.org>
2022-05-29 22:40:29 +0200

    Small annotations on how to use the challenges in main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # This is the main function where all the challenges can be run.
+# To run gameofthrees use gameofthrees.game(enter_number)
+# To run lettervaluesum use lettervaluesum.lettersum(enter_word)
 
 from challenges import lettervaluesum, gameofthrees
 
 if __name__ == "__main__":
-    # gameofthrees.game(31337357)
     lettervaluesum.lettersum("microspectrophotometries")
